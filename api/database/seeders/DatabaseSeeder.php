13d90ae8ec84dc9f4cd0eb990bb7277d67535ced
André Lobo <loboandre@hotmail.com>
2025-01-16 11:23:26 -0400

    fix: Seeder and migrations

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -1,6 +1,20 @@
-public function run(): void
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Database\Seeders\AdminUserSeeder; // Importar o seeder AdminUserSeeder
+
+class DatabaseSeeder extends Seeder
 {
-    $this->call([
-        AdminUserSeeder::class,
-    ]);
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Chama o seeder do AdminUser
+        $this->call([
+            AdminUserSeeder::class,
+        ]);
+    }
 }
